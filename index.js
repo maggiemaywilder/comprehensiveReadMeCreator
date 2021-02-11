7d4f749ff999bd02399a083563b74cc5264f146c
Maggie may <maggiemaywilder@gmail.com>
2021-02-11 14:51:17 -0600

    add user input prompts

--- index.js
+++ index.js
@@ -1,20 +1,90 @@
 // TODO: Include packages needed for this application
-const md = require('./utils/generateMarkdown');
+const fse = require('fs-extra');
 const inquirer = require('inquirer');
-const fs = require('fs');
+const md = require('./utils/generateMarkdown');
+
+
 
 // Description, Table of Contents, Installation, Usage, License, Contributing, Tests, Questions
 
 // TODO: Create an array of questions for user input
-const questions = ['What is your Github username?', 'What is your email address?', 'What is the title of your project?', 'What description would you like added?', 'What kind of license should your project have?', 'What command should be run to install dependencies?', 'What does the user need to know about using this repo?', 'What guidelines should the user follow for contribution?'];
+const questions = [
+    'What is your Github username?', 
+    'What is your email address?', 
+    'What is the title of your project?', 
+    'What description would you like added?', 
+    'What kind of license should your project have?', 
+    'What command should be run to install dependencies?', 
+    'What does the user need to know about using this repo?', 
+    'What guidelines should the user follow for contribution?',
+    'How can the user test this project?'
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-
+function writeToFile(data) { 
+    fse.outputFile(' ', data, err =>) {
+// should this be a json? add console.log to say complete.
+// json -> markdown
+}
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+      .prompt([
+        {
+            type:'input',
+            name: 'username',
+            message: questions[0],
+        },
+        {
+            type:'input',
+            name: 'emailAddress',
+            message: questions[1],
+        },
+        {
+            type:'input',
+            name: 'projectTitle',
+            message: questions[2],
+        },
+        {
+            type:'input',
+            name: 'description',
+            message: questions[3],
+        },
+        {
+            type:'list',
+            name: 'license',
+            message: questions[4],
+            choices: ['Apache-2.0', 'BSD-3-Clause', 'BSD-2-Clause', 'GPL-3.0', 'LGPL-3.0', 'MIT', 'MPL-2.0', 'CDDL-1.0', 'EPL-2.0'],
+            },
+        },
+        {
+            type:'input',
+            name: 'dependencies',
+            message: questions[5],
+        },
+        {
+            type:'input',
+            name: 'usageInfo',
+            message: questions[6],
+        },
+        {
+            type:'input',
+            name: 'contribution',
+            message: questions[7],
+        },        {
+            type:'input',
+            name: 'testInstructions',
+            message: questions[8],
+        },
+    ]
+        
+
+    ).then((answers) => {
+
+    });
+}
 
 // Function call to initialize app
 init();
